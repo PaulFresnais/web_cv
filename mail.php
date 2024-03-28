0f7e2f36ad18c2d25f3fe55cf0db8eb63c92971a
PaulFresnais <paul.fresnais@gmail.com>
2024-03-28 22:01:55 +0100

    no_form

--- mail.php
+++ mail.php
@@ -1,4 +1,14 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 $name = trim($_POST['contact-name']);
 $phone = trim($_POST['contact-phone']);
 $email = trim($_POST['contact-email']);
@@ -30,19 +40,57 @@ if ($name == "") {
 } else {
     $to = 'paul.fresnais@gmail.com';
     $subject = 'Prise de contact - webcv';
-    $_message = '<html><head></head><body>';
-    $_message .= '<p>Name: ' . $name . '</p>';
-    $_message .= '<p>Message: ' . $phone . '</p>';
-    $_message .= '<p>Email: ' . $email . '</p>';
-    $_message .= '<p>Message: ' . $message . '</p>';
-    $_message .= '</body></html>';
+    $_message .= $name;
+    $_message .= $phone;
+    $_message .= $email;
+    $_message .= $message;
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:  inbio <contact@example.com>' . "\r\n";
-    mail($to, $subject, $_message, $headers, '-f paul.fresnais@gmail.com');
+    $headers .= 'From:' . $email ."\r\n";
+    mail($to, $subject, $_message, $headers);
 
     $msg['success'] = "\n Email has been sent successfully.";
     $msg['code'] = TRUE;
 }
 echo json_encode($msg);
+
+function sendmail ($to, $_message, $headers) {
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = '';                                     //SMTP username
+        $mail->Password   = '';                                     //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom($email, 'Mailer');
+        $mail->addAddress('paul.fresnais@gmail.com');               //Add a recipient
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $headers;
+        $mail->Body    = $_message;
+        //$mail->AltBody = '';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+}
